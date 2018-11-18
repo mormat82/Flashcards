d0addbe188f3f92b7426e3e0e6f576a80c47da7c
mormat82 <m2108m@gmail.com>
2018-11-18 15:36:48 +0100

    strona gówna dla niezalogowanych

--- account/views.py
+++ account/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .forms import UserRegistrationForm
 
-@login_required
+
 def dashboard(request):
     return render(request,
                   'account/dashboard.html',
